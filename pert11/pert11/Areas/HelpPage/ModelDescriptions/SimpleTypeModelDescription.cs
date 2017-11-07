e666b31d40c5acece49a9516196cf9d501cccbf4
Developer <Developer@ADV2-18>
2017-11-07 20:02:51 +0700

    Initial Project

---
+++ pert11/pert11/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace pert11.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ pert11/pert11/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="pert11.WebApiApplication" Language="C#" %>
